f46b64312d1f7abd90eb3bcfcad39d9d9b6b8b32
yorukuma <blackmax1886@gmail.com>
2021-07-18 11:46:08 +0900

    add fmt.Scan test for AddTask func with 1 warning in testcode in line29 (unhandled error)

--- cmd/task/task.go
+++ cmd/task/task.go
@@ -1,6 +1,9 @@
 package task
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type Task struct {
 	Name    string
@@ -11,6 +14,17 @@ func GetTask(task Task) string {
 	return fmt.Sprintf(`%v : %v`, task.Name, task.Content)
 }
 
-func AddTask() string {
-	return ""
+func AddTask(input *os.File) Task {
+	if input == nil {
+		input = os.Stdin
+	}
+
+	var name, content string
+	fmt.Println("Enter the name & content of your task\nname content =")
+	_, err := fmt.Fscanf(input, "%s %s", &name, &content)
+	if err != nil {
+		panic(err)
+	}
+
+	return Task{Name: name, Content: content}
 }
--- cmd/task/task_test.go
+++ cmd/task/task_test.go
@@ -2,15 +2,17 @@ package task_test
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"team_task/cmd/task"
 	"testing"
 )
 
 func TestGetTask(t *testing.T) {
-	sample_task := task.Task{Name: "sample_name", Content: "sample_content"}
+	sampleTask := task.Task{Name: "sample_name", Content: "sample_content"}
 	//output := task.GetTask("name", "content")
-	output := task.GetTask(sample_task)
-	want := fmt.Sprintf(`%v : %v`, sample_task.Name, sample_task.Content)
+	output := task.GetTask(sampleTask)
+	want := fmt.Sprintf(`%v : %v`, sampleTask.Name, sampleTask.Content)
 	if output != want {
 		t.Errorf("error")
 	}
@@ -18,8 +20,28 @@ func TestGetTask(t *testing.T) {
 
 func TestAddTask(t *testing.T) {
 	//	ToDo: 標準入力→受け取った内容をチェック
-	output := task.AddTask()
-	if output != "1" {
-		t.Errorf(`unexpected value error : %v, expected 1`, output)
+	var sampleTask = task.Task{}
+	// create temp file for sample Task data
+	input, err := ioutil.TempFile("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer input.Close()
+
+	// write sample Task data to temp
+	_, err = io.WriteString(input, "codetest"+" "+"create AddTask function and test it\n")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// fileの最初を原点にwhence=0, offset=0から次のReadWriteを返す
+	_, err = input.Seek(0, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sampleTask = task.AddTask(input)
+	if sampleTask.Name != "codetest" && sampleTask.Content != "create AddTask function and test it" {
+		t.Errorf(`unexpected value error: {Name:%v, Content:%v}`, sampleTask.Name, sampleTask.Content)
 	}
 }
